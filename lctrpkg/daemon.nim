9e1045608a95bea5ef1aaff2486987d49c5239a3
josedalves <>
2018-02-23 17:59:16 +0000

    Daemon: change threading implementation

--- lctrpkg/daemon.nim
+++ lctrpkg/daemon.nim
@@ -11,8 +11,12 @@ import db_sqlite
 import strutils
 import parseopt2
 import pegs
+import system
 
 var process_lock : Lock
+var process_cond : Cond
+var process_channel : Channel[inotify.InotifyEvent]
+
 var running : bool = false
 
 const
@@ -25,7 +29,8 @@ const
     pinotify.IN_MOVE or
     pinotify.IN_DELETE_SELF or
     pinotify.IN_MOVE_SELF or
-    pinotify.IN_MODIFY
+    pinotify.IN_MODIFY or
+    pinotify.IN_EXCL_UNLINK
   )
 
 
@@ -50,53 +55,73 @@ proc myStat(dir : string) : Stat =
     echo strerror(errno)
     raise newException(Exception, "Bad stat")
 
-proc processThread(config : LCTRConfig,  events : seq[MyInotifyEvent]) = 
-  process_lock.acquire()
+proc processThread(config : LCTRConfig) = 
+
   var fullpath : string
   var db_conn : LCTRDBConnection = newLCTRDBConnection(config.dbPath)
-  db_conn.acquireDBLock(retries=60, timeout=1000)
+  var event : inotify.InotifyEvent
 
-  for e in events:
-    if e.name == nil:
-      continue
-    else:
-      fullpath = joinPath(e.path, e.name)
+  process_channel.open()
 
-    try:
+  while running:
 
-      if (e.mask and pinotify.IN_ISDIR) != 0:
-        #TODO : Handle directories
+    wait(process_cond, process_lock)
+
+    db_conn.acquireDBLock(retries=60, timeout=1000)
+
+    var cdata = process_channel.tryRecv()
+
+    while cdata.dataAvailable:
+      event = cdata.msg
+
+      if event == nil:
+        echo "ABORT!"
+        return
+
+      if event.name == nil:
         continue
+      else:
+        fullpath = joinPath(event.path, event.name)
 
-      if (e.mask and pinotify.IN_ATTRIB) != 0:
-        echo "Attributes changed for $1" % fullpath
-        db_conn.updateObject(newLCTRAttributes(e.name, e.path, myStat(fullpath)))
-      elif (e.mask and pinotify.IN_CLOSE_WRITE) != 0:
-        echo "Close write for $1" % fullpath
-        db_conn.updateObject(newLCTRAttributes(e.name, e.path, myStat(fullpath)))
-      elif (e.mask and pinotify.IN_CREATE) != 0:
-        echo "Create $1" % fullpath
-        db_conn.addOrReplaceObject(newLCTRAttributes(e.name, e.path, myStat(fullpath)))
-      elif (e.mask and pinotify.IN_DELETE) != 0:
-        echo "Delete $1" % fullpath
-        db_conn.delObject(e.name, e.path)
-      elif (e.mask and pinotify.IN_MODIFY) != 0:
-        echo "Modify $1" % fullpath
-        db_conn.updateObject(newLCTRAttributes(e.name, e.path, myStat(fullpath)))
-      elif (e.mask and pinotify.IN_DELETE_SELF) != 0:
-        discard
-      elif (e.mask and pinotify.IN_MOVE_SELF) != 0:
-        discard
-      elif (e.mask and pinotify.IN_MOVE) != 0:
-        echo "Move $1" % fullpath
-    except DBError:
-      raise
-    except:
-      discard
+      try:
+        if (event.mask and pinotify.IN_ISDIR) != 0:
+          #TODO : Handle directories
+          continue
 
-  db_conn.releaseDBLock()
+        if (event.mask and pinotify.IN_ATTRIB) != 0:
+          echo "Attributes changed for $1" % fullpath
+          db_conn.updateObject(newLCTRAttributes(event.name, event.path, myStat(fullpath)))
+        elif (event.mask and pinotify.IN_CLOSE_WRITE) != 0:
+          echo "Close write for $1" % fullpath
+          db_conn.updateObject(newLCTRAttributes(event.name, event.path, myStat(fullpath)))
+        elif (event.mask and pinotify.IN_CREATE) != 0:
+          echo "Create $1" % fullpath
+          db_conn.addOrReplaceObject(newLCTRAttributes(event.name, event.path, myStat(fullpath)))
+        elif (event.mask and pinotify.IN_DELETE) != 0:
+          echo "Delete $1" % fullpath
+          db_conn.delObject(event.name, event.path)
+        elif (event.mask and pinotify.IN_MODIFY) != 0:
+          echo "Modify $1" % fullpath
+          db_conn.updateObject(newLCTRAttributes(event.name, event.path, myStat(fullpath)))
+        elif (event.mask and pinotify.IN_DELETE_SELF) != 0:
+          discard
+        elif (event.mask and pinotify.IN_MOVE_SELF) != 0:
+          discard
+        elif (event.mask and pinotify.IN_MOVE) != 0:
+          echo "Move $1" % fullpath
+      except DBError:
+        raise
+      except:
+        discard
+
+      # get next cdata
+      cdata = process_channel.tryRecv()
+
+    db_conn.releaseDBLock()
+
+  process_channel.close()
   db_conn.close()
-  process_lock.release()
+  #process_lock.release()
 
 proc globToPEG*(pattern : string) : string =
   var word : seq[char] =  @[]
@@ -173,50 +198,38 @@ proc setupWatchers(i : InotifyManager, specs : seq[MonitorSpec], abortOnError : 
 
 proc monitor(config : LCTRConfig, monitorSpecs : seq[MonitorSpec]) = 
   var im = newInotifyManager()
-  #var process : Thread[tuple[directory : string, events : seq[MyInotifyEvent]]]
   var cache : seq[MyInotifyEvent] = @[]
   running = true
 
   var db = newLCTRDBConnection(config.dbPath)
-  #{.gcsafe.}:
-  #  for monitor_spec in db.getMonitors():
-  #    echo "Adding monitor: " & monitor_spec.path
-  #    discard im.addWatcher(monitor_spec.path, pinotify.IN_ALL_EVENTS, monitor_spec.recursive)
   db.close()
-
-  #echo expandFilename("~/work")
   echo "Initializing watchers"
-
   setupWatchers(im, monitorSpecs)
-
-  #for spec in monitorSpecs:
-  #  try:
-  #    echo spec.path, " ", spec.depth, " ", spec.exclude
-  #    im.addWatcher(expandTilde(spec.path), INOTIFY_EVENTS, true, spec.depth, spec.exclude)
-  #  except InotifyWatcherException as e:
-  #    if e.error == ENOSPC:
-  #      echo "Failed to create all watchers. Please increase watch limit with 'echo n > /proc/sys/fs/inotify/max_user_watches'"
-  #    else:
-  #      raise
-
   echo "Starting daemon"
 
+  spawn processThread(config)
+  process_channel.open()
+
+  var event : inotify.InotifyEvent
   while running:
     try:
-      let ie = im.readEvent(1000)
-      if joinPath(ie.path, ie.name) ==  expandFilename(config.dbPath):
+      event = im.readEvent(1000)
+      if joinPath(event.path, event.name) ==  expandFilename(config.dbPath):
         #HACK: filter out db file
         continue
-      cache.add(ie)
+      #cache.add(ie)
     except Exception:
-      discard
-      
-    if cache.len > 0 and process_lock.tryAcquire():
-      let c = cache
-      cache = @[]
-      spawn processThread(config, c)
-      process_lock.release()
-  process_lock.release()
+      continue
+      #raise
+
+    withLock(process_lock):
+      process_channel.send(event)
+      process_cond.signal()
+
+  process_channel.close()
+  #process_lock.release()
+  process_cond.signal()
+  #sync()
   im.close()
 
 proc handleTERM(i : cint) = 
@@ -225,8 +238,6 @@ proc handleTERM(i : cint) =
 proc handleINT(i : cint) = 
   running = false
 
-# lctr daemon 'monitor:/foo/bar depth:2 exclude:.*'
-# lctr daemon --monitor:'/home/sup' --exclude:'ff' --exclude:'ff' --limit:1 --monitor:
 proc modeDaemon*(config : LCTRConfig, op : var OptParser) = 
   # signal handler:
   signal(SIGTERM, handleTERM)
